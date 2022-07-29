dbd4175a08fde340399e42b57ad8208886feac34
fukutaniryousuke <Fr83653554@gmail.com>
2022-07-29 06:03:11 +0000

    adminHome追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   sessions: "admin/sessions"
 }
 
+  get "/admin" => "admin/homes#top"
+
+  namespace :admin do
+  end
   devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
