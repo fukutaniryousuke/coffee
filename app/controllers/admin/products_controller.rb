db781ec095add06cf48a828be21af2092872c84e
fukutaniryousuke <Fr83653554@gmail.com>
2022-07-31 08:29:09 +0000

    productnew作成

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,21 +1,35 @@
 class Admin::ProductsController < ApplicationController
-  
+
   def index
   end
-  
+
   def new
+    @product = Product.new
   end
-  
+
   def show
   end
-  
+
   def edit
   end
-  
+
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_products_path
+      flash[:notice] = "商品の登録が完了しました"
+    else
+      render "new"
+    end
   end
-  
+
   def update
   end
-  
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :introduction, :ganre_id, :price, :image, :is_active)
+  end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,10 @@
 class Product < ApplicationRecord
   has_one_attached :image
-  
+
   belongs_to :ganre
+
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :image, presence: true
+  validates :price, presence: true
 end
