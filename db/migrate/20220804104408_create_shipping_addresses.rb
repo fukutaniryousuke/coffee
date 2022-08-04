6c8081df9c69340c146175726e443c38136e8ce4
fukutaniryousuke <Fr83653554@gmail.com>
2022-08-04 10:47:01 +0000

    カラム追加

--- db/migrate/20220804104408_create_shipping_addresses.rb
+++ db/migrate/20220804104408_create_shipping_addresses.rb
@@ -1,6 +1,10 @@
 class CreateShippingAddresses < ActiveRecord::Migration[6.1]
   def change
     create_table :shipping_addresses do |t|
+      t.string :postcode
+      t.string :address
+      t.string :name
+      t.integer :customer_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_31_070839) do
+ActiveRecord::Schema.define(version: 2022_08_04_104408) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -84,6 +84,15 @@ ActiveRecord::Schema.define(version: 2022_07_31_070839) do
     t.integer "ganre_id"
     t.integer "price"
     t.boolean "is_active", default: true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "shipping_addresses", force: :cascade do |t|
+    t.string "postcode"
+    t.string "address"
+    t.string "name"
+    t.integer "customer_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
