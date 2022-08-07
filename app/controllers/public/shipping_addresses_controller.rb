6db3166b7c010d841ca756849da36f55f9f03f81
fukutaniryousuke <Fr83653554@gmail.com>
2022-08-07 06:32:21 +0000

    controller

--- app/controllers/public/shipping_addresses_controller.rb
+++ app/controllers/public/shipping_addresses_controller.rb
@@ -1,8 +1,25 @@
 class Public::ShippingAddressesController < ApplicationController
   
   def index
+    @shipping_address = ShippingAddress.new
+  end
+  
+  def create
+    @shipping_address = ShippingAddress.new(shipping_address_params)
+    if @shipping_address.save
+      redirect_to shipping_addresses_path
+      flash[:notice] = "登録が完了しました"
+    else
+      render "index"
+    end
   end
   
   def edit
+  end
+  
+  private
+  
+  def shipping_address_params
+    params.require(:shipping_address).permit(:postcode, :address, :name, :customer_id)
   end
 end
